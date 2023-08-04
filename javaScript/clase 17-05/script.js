ae388839afee0f64e72c9666bbfbee79f1a41aa5
Santino Testa <santidonatotesta@gmail.com>
2023-08-04 15:42:34 -0300

    clase 17-05 añadida

---
+++ javaScript/clase 17-05/script.js
@@ -0,0 +1,21 @@
+/*let sourceDiv = document.getElementById("source")
+let destinationDiv = document.getElementById("destination")
+
+let tempContent = sourceDiv.textContent
+
+sourceDiv.textContent = destinationDiv.textContent
+destinationDiv.textContent = tempContent
+
+let titulo = document.getElementById("titulo")
+
+*/
+
+let myDiv = document.getElementById("myDiv")
+
+
+myDiv.style.width = "200px"
+myDiv.style.height = "200px"
+myDiv.style.backgroundColor = "red"
+
+
+myDiv.classList.add("square")
